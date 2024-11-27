a197bbc0c6b6500a2e08367e11e517c70ec71904
yebali <kwonyebali@gmail.com>
2024-11-27 17:18:50 +0900

    feat: 의존성 버전 변경

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -12,23 +12,22 @@ repositories {
 
 dependencies {
     // gRPC 및 Protobuf 관련 의존성
-    implementation("io.grpc:grpc-netty:1.57.2")
     implementation("io.grpc:grpc-protobuf:1.57.2")
     implementation("io.grpc:grpc-stub:1.57.2")
     implementation("io.grpc:grpc-kotlin-stub:1.4.1")
 
     // Protocol Buffers (Protobuf)
-    implementation("com.google.protobuf:protobuf-java:4.28.2")
+    implementation("com.google.protobuf:protobuf-java:4.28.3")
 
     // Kotlin 런타임 및 유틸리티
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("net.devh:grpc-spring-boot-starter:2.15.0.RELEASE")
+    implementation("net.devh:grpc-spring-boot-starter:3.1.0.RELEASE")
 }
 
 protobuf {
     protoc {
-        artifact = "com.google.protobuf:protoc:4.28.2"
+        artifact = "com.google.protobuf:protoc:4.28.3"
     }
     plugins {
         create("grpc") {
